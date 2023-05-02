c3070425a9327db407e81518ee266c68324016c2
Ali Adrian <alinasrat95@gmail.com>
2023-05-02 17:06:11 +0200

    .

---
+++ PersistentBugger/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PersistentBugger
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Persistence(25));
+		}
+
+		public static int Persistence(long n)
+		{
+			int counter = 0;
+			while (n.ToString().Length > 1)
+			{
+				long r = 1;
+				foreach (char c in n.ToString())
+				{
+					int x = c - '0';
+					r *= x;
+				}
+				n = r;
+				counter++;
+			}
+			return counter;
+		}
+	}
+}
---
+++ RotatePaper/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RotatePaper
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(RotatePaper("56789"));
+		}
+
+		//public static void Check()
+		//{
+		//	RotatePaper()
+		//}
+
+		public static bool RotatePaper(string number)
+		{
+			for (int j = 0; j < number.Length; j++)
+			{
+				if (number.Contains("1") || number.Contains("3") || number.Contains("4") || number.Contains("5") || number.Contains("7"))
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			return true;
+		}
+	}
+}
