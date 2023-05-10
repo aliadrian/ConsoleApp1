dfe6d10df311b04e3a20565036d74515116cc0ed
Ali Adrian <alinasrat95@gmail.com>
2023-05-10 14:08:59 +0200

    Added function called SumsOfParts

---
+++ SumsOfParts/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SumsOfParts
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(PartsSums(new int[] { }));
+		}
+		// My way of writing it 
+		//public static int[] PartsSums(int[] ls)
+		//{
+		//	List<int> list = new List<int>();
+
+		//	for (int i = 0; i < ls.Length + 1; i++)
+		//	{
+		//		int sum = 0;
+		//		for (int j = i; j < ls.Length; j++)
+		//		{
+		//			sum += ls[j];
+		//		}
+		//		list.Add(sum);
+		//	}
+
+		//	return list.ToArray();
+		//}
+
+		// Updated version of the code optimized
+		public static int[] PartsSums(int[] ls)
+		{
+			int n = ls.Length;
+			int[] result = new int[n + 1];
+			int sum = 0;
+
+			for (int i = n - 1; i >= 0; i--)
+			{
+				sum += ls[i];
+				result[i] = sum;
+			}
+
+			return result;
+		}
+	}
+}
