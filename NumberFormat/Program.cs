94da9efdde61a796a0257c7d1b9e872b3ca4c7f6
Ali Adrian <alinasrat95@gmail.com>
2023-05-22 15:43:49 +0200

    NumberFormat and Scramblies functions

---
+++ NumberFormat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace NumberFormat
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(NumberFormat(-100000));
+		}
+
+		public static string NumberFormat(int number)
+		{
+			return Regex.Replace(number.ToString(), "(\\d)(?=(\\d{3})+$)", "$1,");
+		}
+	}
+}
---
+++ Scramblies/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Scramblies
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Scramble("aabbcamaomsccdd", "comma"));
+		}
+
+		public static bool Scramble(string str1, string str2)
+		{
+			string word = string.Empty;
+			for (int i = 0; i < str2.Length; i++)
+			{
+				if (str1.Contains(str2[i]))
+				{
+					word += str2[i];
+					str1 = str1.Remove(str1.IndexOf(str2[i]), 1);
+				}
+			}
+
+			if (word == str2)
+				return true;
+			else
+				return false;
+		}
+	}
+}
